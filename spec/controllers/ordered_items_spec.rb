ddc55d5c114f51f61cc8ccf3506a233e53500ce6
devamitra16 <devamitra.t2020cse@sece.ac.in>
2023-04-12 21:05:39 +0530

    files added

--- spec/controllers/ordered_items_spec.rb
+++ spec/controllers/ordered_items_spec.rb
@@ -1,9 +1,15 @@
-require 'rails_helper'
-RSpec.describe OrderedItemsController do
-	describe "OrderedItems #index" do
-		
-			
+# require 'rails_helper'
+# RSpec.describe OrderedItemsController do
+# 	describe "OrderedItems #index" do
+# 	   context "GET #index" do
+# 		let(:user){create(:user)}
+# 		let(:instrum){create(:instrument,user: user)}
+# 		it "assigns all instruments as @instruments" do
+# 			get :index
+# 			expect(assigns(:instruments)).to eq([instrument])
+# 		end
+# 	end	
         
-    end
+#     end
     
-end
+# end
